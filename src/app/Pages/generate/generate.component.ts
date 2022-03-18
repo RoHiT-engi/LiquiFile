4b0c04456e42b6a8de8679fb146b1f6d01a64eba
RoHiT-engi <79554502+RoHiT-engi@users.noreply.github.com>
2022-03-18 20:21:11 +0530

    Updated Generator

--- src/app/Pages/generate/generate.component.ts
+++ src/app/Pages/generate/generate.component.ts
@@ -17,29 +17,27 @@ export class GenerateComponent implements OnInit {
     FileValidators.maxFileCount(2)]
   );
 
-  
 
   onValueChange(file: File[]) {
     for (let index in file) {
       console.log(index);
-      if(file[index].size > 100000000) {
-        alert("File size is too big");
-        delete file[index];
-        this.fileControl.setValue(file);
-      }
-      if(file[index].type == "video/*" || file[index].type == "audio/*") {
-        alert("File type is not supported");
-        delete file[index];
-        this.fileControl.setValue(file);
-      }
-      var y: number = +index;
-      if(y >= 5) {
-        alert("File Limit Exceeded");
-        delete file[index];
-      }
+      // if(file[index].size > 100000000) {
+      //   alert("File size is too big");
+      //   // delete file[index];
+      //   file.splice(file.length, 1);
+      //   this.fileControl.setValue(file);
+      // }
+      // if(file[index].type == "video/*" || file[index].type == "audio/*") {
+      //   alert("File type is not supported");
+      //   // delete file[index];
+      //   file.splice(file.length, 1);
+      //   this.fileControl.setValue(file);
+      // }
     }
-
   }
+
+
+
   ngOnInit(): void {
     this.fileControl.valueChanges.subscribe((files: File[]) =>
       console.log("hello there",this.fileControl.value, this.fileControl.valid)
