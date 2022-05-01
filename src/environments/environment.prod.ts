c0325ae605988fd1af01275e475d7442ab4f545b
rohit vichare <79554502+RoHiT-engi@users.noreply.github.com>
2022-05-01 09:01:45 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDcMZvXyleiufKfwTQ_ZKta9ML8Jtepru4",
-    authDomain: "friendlychat-9a96f.firebaseapp.com",
-    databaseURL: "https://friendlychat-9a96f-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "friendlychat-9a96f",
-    storageBucket: "friendlychat-9a96f.appspot.com",
-    messagingSenderId: "701977005955",
-    appId: "1:701977005955:web:6882995f814407344470b6",
-    measurementId: "G-431KJ7BLD4"
+    apiKey: "//add your key",
+    authDomain: "//add your key",
+    databaseURL: "//add your key",
+    projectId: "//add your key",
+    storageBucket: "//add your key",
+    messagingSenderId: "//add your key",
+    appId: "//add your key",
+    measurementId: "//add your key"
   }
 };
